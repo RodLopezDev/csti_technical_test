c3c153566445fd302274e29cd7144f3ce637f50b
RodLopezDev <rodrigolopezdev@gmail.com>
2024-01-06 21:33:59 -0500

    feat: :sparkles: added jwt token generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: () => ({
+        secret: '12345678',
+        signOptions: { expiresIn: '600s' },
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtService } from '@nestjs/jwt';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 
+import { LoginAuthDto } from './dto/login-auth.dto';
+
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly jwtService: JwtService) {}
+
   @Post()
-  login(@Body() createAuthDto: CreateAuthDto) {
-    return true;
+  login(@Body() dto: LoginAuthDto) {
+    const isLoggin = dto.user === 'RODRIGO' && dto.password === 'test-to-csti';
+    if (!isLoggin) {
+      throw new UnauthorizedException('BAD_CREDENTIALS');
+    }
+
+    return this.jwtService.sign({ user: dto.user });
   }
 }
