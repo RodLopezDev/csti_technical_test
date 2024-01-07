2a24e8be428123399e14bc02b94dac077003b9cb
RodLopezDev <rodrigolopezdev@gmail.com>
2024-01-06 21:43:21 -0500

    feat: :sparkles: added exchange-rate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { AuthModule } from './auth/auth.module';
+import { ExchangeRateModule } from './exchange-rate/exchange-rate.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
       }),
     }),
     AuthModule,
+    ExchangeRateModule,
   ],
   controllers: [],
   providers: [],
