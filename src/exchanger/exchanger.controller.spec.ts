7d8a01a6aa0c1b6def33159f0a6ad4846afdd4c9
RodLopezDev <rodrigolopezdev@gmail.com>
2024-01-06 22:01:01 -0500

    test: :rotating_light: added test for exchanger module

--- src/exchanger/exchanger.controller.spec.ts
+++ src/exchanger/exchanger.controller.spec.ts
@@ -1,19 +1,65 @@
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { ExchangerController } from './exchanger.controller';
+import { EnvBootstrap, JwtConfig } from '../common/env-bootstrap';
+import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
+import { ExchangeRateService } from '../exchange-rate/exchange-rate.service';
 
 describe('ExchangerController', () => {
   let controller: ExchangerController;
+  let exchangeRateService: ExchangeRateService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(EnvBootstrap),
+        JwtModule.registerAsync(JwtConfig),
+        ExchangeRateModule,
+      ],
       controllers: [ExchangerController],
       providers: [],
     }).compile();
 
     controller = module.get<ExchangerController>(ExchangerController);
+    exchangeRateService = module.get<ExchangeRateService>(ExchangeRateService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should fail with 404', async () => {
+    const payload = {
+      amount: 1,
+      sourceCurrency: 'USD',
+      targetCurrency: 'PEN',
+    };
+    try {
+      controller.exchange(payload);
+    } catch (e) {
+      expect(e?.message).toBe('EXCHANGE_RATE_NOT_FOUND');
+    }
+  });
+
+  it('should fail with 404', async () => {
+    const payload = {
+      amount: 12,
+      sourceCurrency: 'USD',
+      targetCurrency: 'PEN',
+    };
+    const rateExample = {
+      rate: 10,
+      sourceCurrency: 'USD',
+      targetCurrency: 'PEN',
+    };
+
+    jest.spyOn(exchangeRateService, 'findOne').mockReturnValueOnce(rateExample);
+
+    const result = controller.exchange(payload);
+    expect(result.amount).toBe(payload.amount);
+    expect(result.exchangeRate).toBe(rateExample.rate);
+    expect(result.exchangedAmount).toBe(rateExample.rate * payload.amount);
   });
 });
